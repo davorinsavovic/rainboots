f48de874e70f3a0bd5c1fb60bed3af443ca5197d
davorinsavovic <davorinsavovic@gmail.com>
2026-04-01 17:15:28 -0700

    portfolio

--- src/services/emailTemplateService.js
+++ src/services/emailTemplateService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001';
+  import.meta.env.VITE_API_BASE_URL || 'https://api.rainbootsmarketing.com';
 
 export const emailTemplateService = {
   async getAll() {
